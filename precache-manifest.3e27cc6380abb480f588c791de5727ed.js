e5976fc8db28512eb18a19b16d7b71385f61d2f2
Jerson Brito <jersonb@MacBook-Air-de-Jerson.local>
2020-07-25 21:15:00 -0300

    Updates

---
+++ precache-manifest.3e27cc6380abb480f588c791de5727ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf12bb61f6f354b2b412a375dac36650",
+    "url": "/jersonb.github.io/index.html"
+  },
+  {
+    "revision": "5bd22bc9cd71ffa2930c",
+    "url": "/jersonb.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00b11ca079f461da879a",
+    "url": "/jersonb.github.io/static/js/2.8440d837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jersonb.github.io/static/js/2.8440d837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bd22bc9cd71ffa2930c",
+    "url": "/jersonb.github.io/static/js/main.55102686.chunk.js"
+  },
+  {
+    "revision": "037adce60b207a5cac00",
+    "url": "/jersonb.github.io/static/js/runtime-main.188eb2f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jersonb.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
