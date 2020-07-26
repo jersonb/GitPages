3334a526b07f4eeba724429ea6783a161a004d29
Jerson Brito <jersonb@MacBook-Air-de-Jerson.local>
2020-07-25 21:51:51 -0300

    Updates

---
+++ precache-manifest.dc0afdeb65e18611dca01f31f657c4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b646d7eed3872f3684f7fdee4f0bf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a59040261be1fa0f0bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b8655376afa9945d5bf",
+    "url": "/static/js/2.33c4fe95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.33c4fe95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a59040261be1fa0f0bb",
+    "url": "/static/js/main.d0702978.chunk.js"
+  },
+  {
+    "revision": "1309b7e636cb4c083b2b",
+    "url": "/static/js/runtime-main.4aa36bc1.js"
+  }
+]);
